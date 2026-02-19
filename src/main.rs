9612c542c5501443b91f614bde2df941639e21b3
Rammy Aly <rammyaly@gmail.com>
2026-02-19 17:58:19 -0600

    first commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+
+// Rust Book Chapter 12 Project
+// minigrep
+
+use std::env;
+use std::fs;
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	dbg!(&args);
+
+	let _prog_self = &args[0];
+	let query = &args[1];
+	let file_path = &args[2];
+
+	println!("Searching for `{query}` in file `{file_path}`");
+
+	let contents = fs::read_to_string(file_path)
+		.expect("Should have been able to read the file");
+
+	println!("With text:\n{contents}");
+}
+
+
+
+
+
+
+
+
---
+++ target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/home/itsrxmmy/Desktop/learn-rust/projects/minigrep/target/debug/minigrep: /home/itsrxmmy/Desktop/learn-rust/projects/minigrep/src/main.rs
