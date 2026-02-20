202a089c283acc043b6edaf3e35dae9d8e26da68
Rammy Aly <rammyaly@gmail.com>
2026-02-19 21:10:51 -0600

    replaced Config::new constructor with Config::build to handle errors more effectively and to make errors more user-friendly

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
@@ -13,7 +14,12 @@ fn main() {
 	// let query = &args[1];
 	// let file_path = &args[2];
 
-	let config = Config::new(&args);
+	// let config = Config::new(&args);
+
+	let config = Config::build(&args).unwrap_or_else(|err| {
+		println!("Problem parsing arguments: {err}");
+		process::exit(1);
+	});
 
 	println!("Searching for `{}` in file `{}`", config.query, config.file_path);
 
@@ -28,12 +34,31 @@ struct Config {
 	file_path: String,
 }
 
+
+
 impl Config {
+	/*
 	fn new(args: &[String]) -> Config {
+		if args.len() < 3 {
+			panic!("not enough arguments");
+		}
+
 		let query = args[1].clone();
 		let file_path = args[2].clone();
 
 		Config { query, file_path }
+	}
+	*/
+
+	fn build(args: &[String]) -> Result<Config, &'static str> {
+		if args.len() < 3 {
+			return Err("not enough arguments");
+		}
+
+		let query = args[1].clone();
+		let file_path = args[2].clone();
+
+		Ok(Config { query, file_path })
 	}
 }
 
