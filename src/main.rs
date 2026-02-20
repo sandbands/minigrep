097e46857d3bbfaf3315d01e0be5005acdcfd918
Rammy Aly <rammyaly@gmail.com>
2026-02-19 18:57:15 -0600

    added Config Struct to store config values like query and file_path

--- src/main.rs
+++ src/main.rs
@@ -13,22 +13,26 @@ fn main() {
 	// let query = &args[1];
 	// let file_path = &args[2];
 
-	let (query, file_path) = parse_config(&args);
+	let config = parse_config(&args);
 
-	println!("Searching for `{query}` in file `{file_path}`");
+	println!("Searching for `{}` in file `{}`", config.query, config.file_path);
 
-	let contents = fs::read_to_string(file_path)
+	let contents = fs::read_to_string(config.file_path)
 		.expect("Should have been able to read the file");
 
 	println!("With text:\n{contents}");
 }
 
+struct Config {
+	query: String,
+	file_path: String,
+}
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-	let query = &args[1];
-	let file_path = &args[2];
+fn parse_config(args: &[String]) -> Config {
+	let query = args[1].clone();
+	let file_path = args[2].clone();
 
-	(query, file_path)
+	Config { query, file_path }
 }
 
 
